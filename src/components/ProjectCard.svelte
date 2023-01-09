7d0c920d32d88c406f3fa66ed98d7c953384c9dc
Sanjay Dookhoo <sanjaydookhoo@msn.com>
2023-01-09 14:58:23 -0400

    ProjectCard hover added

--- src/components/ProjectCard.svelte
+++ src/components/ProjectCard.svelte
@@ -5,29 +5,60 @@
 	export let project;
 
 	const { imgSrc, name, description, tagList, view, source } = project;
+
+	let sourceExpand = false,
+		cardHovered = false;
 </script>
 
-<div class="w-96 p-2">
-	<div class="rounded-lg border-gray-300 border">
+<div
+	class="w-96 p-2"
+	on:mouseenter={() => (cardHovered = true)}
+	on:mouseleave={() => (cardHovered = false)}
+>
+	<div class="rounded-lg border-gray-300 border hover:shadow-2xl hover:border-2">
 		<div class="w-full h-48">
 			<img class="h-full w-full rounded-t-lg" src={imgSrc} />
 		</div>
 		<div class="w-full divide-y p-4">
-			<div class="h-40">
-				<div class="flex ">
+			<!-- min-height: 16rem is equivalent to min-h-64, even though tailwind doesnt include this class -->
+			<div class={`flex flex-col ${cardHovered ? '' : 'h-64'}`} style="min-height: 16rem;">
+				<div
+					class={`flex flex-wrap ${cardHovered ? '' : 'overflow-hidden'}`}
+					style={cardHovered ? '' : 'height: 30px;'}
+				>
 					{#each tagList as tag, i (tag)}
 						<Chip variant="portfolio" name={tag} />
 					{/each}
 				</div>
 				<div class="text-4xl">{name}</div>
-				<div>{description}</div>
+				<div class={`overflow-hidden flex-grow ${cardHovered ? '' : 'basis-0'}`}>{description}</div>
 			</div>
 			<div class="flex justify-end h-12 pt-2">
 				{#if view}
 					<Button variant="primary" link={true} href={view}>View</Button>
 				{/if}
 				{#if source}
-					<Button variant="secondary" link={true} href={source}>Source</Button>
+					{#if Array.isArray(source)}
+						<!-- <Button variant="secondary" link={true} href={source}>Source</Button> -->
+						<div
+							class="flex"
+							on:mouseenter={() => (sourceExpand = true)}
+							on:mouseleave={() => (sourceExpand = false)}
+						>
+							{#if !sourceExpand}
+								<!-- appears like the normal source button, but only until hovered -->
+								<div class={`px-2 py-1 m-1 rounded cursor-pointer bg-gray-300`}>Source</div>
+							{:else}
+								{#each source as sourceEl, i (sourceEl.title)}
+									<Button variant="secondary" link={true} href={sourceEl.source}
+										>{sourceEl.title}
+									</Button>
+								{/each}
+							{/if}
+						</div>
+					{:else}
+						<Button variant="secondary" link={true} href={source}>Source</Button>
+					{/if}
 				{/if}
 			</div>
 		</div>
--- src/routes/portfolio/+page.svelte
+++ src/routes/portfolio/+page.svelte
@@ -3,18 +3,62 @@
 
 	let projects = [
 		{
-			imgSrc: 'https://simonsankar.github.io/img/messiops2.png',
-			name: 'MessiOps',
-			description: 'A web app dedicated to show the pure god tier brilliance of Lionel Messi.',
-			tagList: ['reactjs', 'nextjs', 'strapi', 'antd', 'less'],
-			view: 'http://www.google.com',
-			source: '/'
+			imgSrc:
+				'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/1280px-Image_created_with_a_mobile_phone.png',
+			name: 'Nodejs Better Console',
+			description:
+				'Testing a node.js script is considerably different than testing javascript in a browser. This package provides additional functionality for debugging node.js via console messages.',
+			tagList: ['nodejs'],
+			view: 'https://www.npmjs.com/package/nodejs-better-console',
+			source: 'https://github.com/SanjayDookhoo/nodejs-better-console'
+		},
+		{
+			imgSrc:
+				'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/1280px-Image_created_with_a_mobile_phone.png',
+			name: 'Vite Plugin Svelte Bind Spread',
+			description:
+				'A vite plugin for svelte that by default, if a object is spread into a component, it will bind each property in the object as props.',
+			tagList: ['vite', 'svelte', 'plugin'],
+			view: 'https://www.npmjs.com/package/vite-plugin-svelte-bind-spread',
+			source: 'https://github.com/SanjayDookhoo/vite-plugin-svelte-bind-spread'
+		},
+		{
+			imgSrc:
+				'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/1280px-Image_created_with_a_mobile_phone.png',
+			name: 'Actions File Manager',
+			description:
+				'A File Manager web component (custom element) that can be implemented in vanilla Javascript or any Javascript framework such as, React, Vue or Angular. One of the most powerful features it provides are, "Custom Actions", where any file or folder can have a custom context menu action to perform any Javascript action. It is also a fully self hosted solution using Docker allowing full ownership of all files and data, ensuring the highest security.',
+			tagList: ['file manager', 's3', 'graphql', 'reactjs', 'nodejs'],
+			view: 'https://actions-file-manager.dev/',
+			// source: 'https://github.com/SanjayDookhoo/actions-file-manager'
+			source: [
+				{
+					title: 'Frontend',
+					source: 'https://github.com/SanjayDookhoo/actions-file-manager'
+				},
+				{
+					title: 'Backend',
+					source: 'https://github.com/SanjayDookhoo/actions-file-manager-backend'
+				}
+			]
+		},
+		{
+			imgSrc:
+				'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/1280px-Image_created_with_a_mobile_phone.png',
+			name: 'TXNWallet',
+			description:
+				'A single wallet dashboard that allows multi blockchain analysis of all owned cryptocurrency and additional note keeping of specific historical transactions.',
+			tagList: ['reactjs', 'nodejs'],
+			view: 'https://txnwallet.me/',
+			source: 'https://github.com/SanjayDookhoo/TXN-Wallet'
 		}
 	];
 </script>
 
 portfolio
 
-{#each projects as project, i (project.name)}
-	<ProjectCard {project} />
-{/each}
+<div class="flex flex-wrap justify-center">
+	{#each projects as project, i (project.name)}
+		<ProjectCard {project} />
+	{/each}
+</div>
