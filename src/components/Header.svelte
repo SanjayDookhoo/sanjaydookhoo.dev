5fe1b48768f75ce7d55a907fb1204e96989ee13b
Sanjay Dookhoo <sanjaydookhoo@msn.com>
2023-01-11 10:56:24 -0400

    cleanup

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,5 +1,5 @@
 <script>
-	import logo from '../assets/logo2.png';
+	import logo from '../assets/logo.png';
 	import github from '../assets/github.svg';
 	import linkedin from '../assets/linkedin.svg';
 
