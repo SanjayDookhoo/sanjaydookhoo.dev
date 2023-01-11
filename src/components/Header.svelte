36ba307d76533b0100d033e749fac54275954be1
Sanjay Dookhoo <sanjaydookhoo@msn.com>
2023-01-10 20:33:03 -0400

    fixed logo

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,9 +1,9 @@
 <script>
-	import logo from '../assets/logo.png';
+	import logo from '../assets/logo2.png';
 </script>
 
 <div class="flex justify-between w-full">
-	<a href="/"><img class="h-12 w-64 logo" src={logo} /></a>
+	<a href="/"><img class="h-12 w-64 mt-2" src={logo} /></a>
 	<!-- <div class="flex items-center">
 		<div>Home</div>
 		<div>Portfolio</div>
