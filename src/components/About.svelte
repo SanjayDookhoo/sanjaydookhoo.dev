69666ffa60557a73b03142c75b749cbb68d963da
Sanjay Dookhoo <sanjaydookhoo@msn.com>
2023-01-10 20:39:12 -0400

    small changes

--- src/components/About.svelte
+++ src/components/About.svelte
@@ -6,11 +6,11 @@
 <div class="flex justify-center items-center py-4 bg-red-300 rounded-lg my-4">
 	<img class="h-32 w-32 m-4" src={code} />
 	<div>
-		<div class="text-4xl">I'm Sanjay Dookhoo</div>
+		<div class="text-4xl">Name: Sanjay Dookhoo</div>
 		<div class="py-4">
 			Hello, I am a young software developer from Trinidad and Tobago, I like to create beautiful
-			and user-friendly user interfaces, I can build from web applications to mobile applications.
-			I'm currently open for work
+			and user-friendly user interfaces, I can build from web applications to mobile applications. I
+			am currently open for work
 		</div>
 		<Button variant="primary" link={true} href="mailto:sanjaydookhoo@msn.com">Get In Touch</Button>
 	</div>
